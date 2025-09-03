95d2429beed730991455adb6a0c3f9a81623f718
micronix <sumitds@gmail.com>
2025-09-03 19:59:04 +0530

    Revert "Update next js package to latest version"
    This reverts commit fadf5754d95ffc6ddd607e43eda3d0bdec927a13.

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,4 +1,3 @@
-
 import React, { Suspense } from "react";
 import dynamic from "next/dynamic";
 
@@ -6,7 +5,19 @@ import { getSortedPostsData } from "@library/posts";
 
 import AppData from "@data/app.json";
 
-import SlidersClient from "@components/client/SlidersClient";
+import ScrollHint from "@layouts/scroll-hint/Index";
+import Divider from "@layouts/divider/Index";
+
+import AboutSection from "@components/sections/About";
+import FeaturesSection from "@components/sections/Features";
+import ScheduleSection from "@components/sections/Schedule";
+import CountersSection from "@components/sections/Counters";
+import CallToActionSection from "@components/sections/CallToAction";
+import LatestPostsSection from "@components/sections/LatestPosts";
+import SubscribeSection from "@components/sections/Subscribe";
+
+const HeroSlider = dynamic( () => import("@components/sliders/Hero"), { ssr: false } );
+const TestimonialSlider = dynamic( () => import("@components/sliders/Testimonial"), { ssr: false } );
 
 export const metadata = {
   title: {
@@ -20,7 +31,39 @@ async function Home() {
 
   return (
     <>
-      <SlidersClient />
+      <div id="tst-dynamic-banner" className="tst-dynamic-banner">
+        <HeroSlider />
+      </div>
+      <div id="tst-dynamic-content" className="tst-dynamic-content">
+        <div className="tst-content-frame">
+          <div className="tst-content-box">
+            <div className="container tst-p-60-0">
+              <ScrollHint />
+              <AboutSection />
+              <Divider />
+              <FeaturesSection />
+              <Divider />
+              <ScheduleSection />
+              <Divider onlyBottom={0} />
+              <CountersSection />
+            </div>
+          </div>
+        </div>
+        <CallToActionSection />
+        <div className="tst-content-frame">
+          <div className="tst-content-box">
+            <div className="container tst-p-60-60">
+              <TestimonialSlider />
+              <Divider onlyBottom={0} />
+              <Suspense fallback={<div>Loading...</div>}>
+                <LatestPostsSection posts={posts} />
+              </Suspense>
+              <Divider onlyBottom={0} />
+              <SubscribeSection />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
